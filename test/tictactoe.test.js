0f8d80405ff3d683631b65208b72b3e46b0df047
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-03-24 21:18:43 +0100

    [Test] Add tests for some game logic functionalities

--- test/tictactoe.test.js
+++ test/tictactoe.test.js
@@ -1,9 +1,24 @@
-import game from '../src/tictactoe'
+import game from '../src/tictactoe';
 
+describe('test checkBox game functionality', () => {
+  it('checkBox method return true for empty position', () => {
+    const board = ['X', '', 'O', 'X', '', '0', 'X', '', '0'];
+    expect(game.checkBox(1, board)).toBeTruthy();
+  });
 
-test("checkBox method return true check for empty position", () => {
+  it('checkBox method return false for filled position', () => {
+    const board = ['X', 'X', 'O', 'X', '', '0', 'X', '', '0'];
+    expect(game.checkBox(1, board)).toBeFalsy();
+  });
+});
 
-
-    expect(game.checkBox(2)).toBeTruthy()
-
-})
+describe('tests checkWin functionality', () => {
+  it('Player 1 as the winner', () => {
+    const board = ['X', 'X', 'X', 'O', '...', 'O', 'X', '...', 'O', '...'];
+    expect(game.checkWin(board)).toBe('Player 1 Wins');
+  });
+  it('Player 1 as the winner different mark position', () => {
+    const board = ['X', 'O', 'X', 'X', '...', 'O', 'X', '...', 'O', '...'];
+    expect(game.checkWin(board)).toBe('Player 1 Wins');
+  });
+});
